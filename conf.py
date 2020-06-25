307367cabd0b729b1529486b6b88a99b0d018790
zxjack <zxjack@gmail.com>
2020-06-25 15:40:16 +0800

    update
    edit enable_jsdelivr

--- conf.py
+++ conf.py
@@ -14,8 +14,8 @@ template = {
     "path": "../Galileo"
 }
 enable_jsdelivr = {
-    "enabled": False,
-    "repo": ""
+    "enabled": True,
+    "repo": "zxjack/Blog-With-GitHub-Boilerplate@gh-pages"
 }
 
 # 站点设置
